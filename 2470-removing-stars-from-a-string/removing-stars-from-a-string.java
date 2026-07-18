9aaf6da3a2662f5a75763c9ba6495d584090e8ed
Sahild-2005 <159606849+Sahild-2005@users.noreply.github.com>
2026-07-18 11:23:10 +0530

    Time: 92 ms (18.87%) | Memory: 48.9 MB (25.47%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.java
@@ -1,33 +1,22 @@
 class Solution {
-    public String reverse(String s){
-        String rev = "";
-
-        for(int i=s.length()-1;i>=0;i--){
-           char ch = s.charAt(i);
-
-           rev = rev+ch;
-        }
-
-        return rev;
-    }
     public String removeStars(String s) {
-        
+
         Stack<Character> st = new Stack<>();
 
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-
-            if(ch=='*'){
+        for (char ch : s.toCharArray()) {
+            if (ch == '*') {
                 st.pop();
+            } else {
+                st.push(ch);
             }
-            else st.push(ch);
         }
 
-        String ans ="";
-        while(!st.isEmpty()){
-            ans = ans+st.pop();
+        StringBuilder ans = new StringBuilder();
+
+        while (!st.isEmpty()) {
+            ans.append(st.pop());
         }
 
-        return reverse(ans);
+        return ans.reverse().toString();
     }
 }
