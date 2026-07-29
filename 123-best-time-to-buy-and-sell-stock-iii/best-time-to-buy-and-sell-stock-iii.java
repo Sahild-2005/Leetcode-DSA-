106f3e3aad036b0fd44ebde12019f21a4a6a7b89
Sahild-2005 <159606849+Sahild-2005@users.noreply.github.com>
2026-07-29 11:32:43 +0530

    Time: 54 ms (39.07%) | Memory: 117.9 MB (32.95%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,37 +1,40 @@
 class Solution {
-    // recursion+memo
-    int fun(int ind, int buy, int cap, int[] arr, int n, int[][][] dp) {
-        // base case 
-        if (cap == 0)
-            return 0;
-        if (ind == n)
-            return 0;
-
-            if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-
-        // recurrence 
-        if (buy == 1) { // buy , not buy
-            dp[ind][buy][cap] = Math.max(-arr[ind] + fun(ind + 1, 0, cap, arr, n,dp),
-                    0 + fun(ind + 1, 1, cap, arr, n,dp));
-        }
-
-        else { // sell  not sell
-             dp[ind][buy][cap] = Math.max(arr[ind] + fun(ind + 1, 1, cap - 1, arr, n,dp),
-                    0 + fun(ind + 1, 0, cap, arr, n,dp));
-        }
-
-        return  dp[ind][buy][cap] ;
-    }
-
+    // tabultaion 
     public int maxProfit(int[] arr) {
-        int n = arr.length;
-        int[][][] dp = new int[n][2][3];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int n = arr.length;
+
+        int[][][] dp = new int[n+1][2][3];
+
+        int ind, buy, cap;
+
+        // 2 base case 
+
+        for (ind = 0; ind < n; ind++)
+            for (buy = 0; buy <= 1; buy++)
+                dp[ind][buy][0] = 0;
+
+        for (buy = 0; buy <= 1; buy++)
+            for (cap = 0; cap <= 2; cap++)
+                dp[n][buy][cap] = 0;
+
+        for (ind = n - 1; ind >= 0; ind--) {
+            for (buy = 0; buy <= 1; buy++) {
+                for (cap = 1; cap <= 2; cap++) {
+                    if (buy == 1) { // buy , not buy
+                        dp[ind][buy][cap] = Math.max(-arr[ind] + dp[ind+1][0][cap],
+                                0 + dp[ind+1][1][cap]);
+                    }
+
+                    else { // sell  not sell
+                        dp[ind][buy][cap] = Math.max(arr[ind] + dp[ind+1][1][cap-1],
+                                0 + dp[ind+1][0][cap]);
+                    }
+                }
             }
         }
-        return fun(0, 1, 2, arr, n, dp);
+
+        return dp[0][1][2];
+
     }
 }
