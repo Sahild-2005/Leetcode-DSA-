5c232a4135ff07d2f789b06b652af8227a0eff8e
Sahild-2005 <159606849+Sahild-2005@users.noreply.github.com>
2026-08-10 22:19:12 +0530

    Time: 691 ms (64.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,9 @@
+select s.user_id , ifnull(Round(sum(action='confirmed')/count(*) ,2),0.00)as confirmation_rate
+from Signups s 
+left join Confirmations c
+on 
+s.user_id = c.user_id
+
+group by 
+s.user_id
+
