2c072d33c4bb2213822b825bf633860613972a9c
Sahild-2005 <159606849+Sahild-2005@users.noreply.github.com>
2026-08-17 19:45:19 +0530

    Time: 2 ms (33.69%) | Memory: 46.3 MB (54.34%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -1,18 +1,72 @@
 class Solution {
-    public int findSpecialInteger(int[] arr) {
+       public int searchRange(int[] arr, int target) {
         int n = arr.length;
 
-        int occ = n / 4;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
+        int[] ans = new int[2];
 
-        for (Integer ele : map.keySet()) {
-            if (map.get(ele) > occ) {
-                return ele;
+        // ll , ul
+
+        int low = 0;
+        int high = n - 1;
+
+        int LL = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+              
+                LL = mid;
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                // search right
+                low = mid + 1;
+
+            } else {
+                // search left
+                high = mid - 1;
             }
         }
+        int UL = -1;
+         low = 0;
+         high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+              
+                UL = mid;
+                low = mid + 1;
+            } else if (arr[mid] < target) {
+                // search right
+                low = mid + 1;
+
+            } else {
+                // search left
+                high = mid - 1;
+            }
+        }
+
+        ans[0] = LL;
+        ans[1] = UL;
+
+       
+        if (LL == -1) {
+            return 0;
+        }
+
+        return UL - LL + 1;
+    }
+    
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length;
+        if(n==1) return arr[0];
+        for(int i=0;i<n;i++){
+            int freq = searchRange(arr,arr[i]);
+            if(freq>n/4) return arr[i];
+        }
+
         return -1;
     }
 }
